a4cd87e7b842a62ed3fc50186403eb600f79f57b
H_Khalid__H <129606136+khaharr@users.noreply.github.com>
2024-05-03 16:29:46 +0200

    Update bdd.ts

--- server/api/bdd/bdd.ts
+++ server/api/bdd/bdd.ts
@@ -1,12 +1,12 @@
-// import pg from 'pg'
-// import useBdd from '~/composables/useBdd';
-// const { Pool, Client } = pg
+import pg from 'pg'
+export default defineEventHandler(async (event) => {
+const {Client } = pg
 
-// const pool = new Pool({
-//   host: 'localhost',
-//   port: 5432,
-//   database: 'tools-iia',
-//   user: 'postgres',
-//   password: 'postgres',
-// })
-// export { pool as default };
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  database: 'tools-iia',
+  user: 'postgres',
+  password: 'postgres',
+})
+})
