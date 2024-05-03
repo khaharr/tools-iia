f85d4a95662b2c3c72264820cc44cee5dafd1d6f
brahimS78 <brahim.sisi78@gmail.com>
2024-05-03 16:23:15 +0200

    Update useBdd.ts

--- composables/useBdd.ts
+++ composables/useBdd.ts
@@ -1,39 +1,35 @@
-// import bdd from '~/server/api/bdd/bdd';
 
-// export default function useBdd() {
-//   const data = ref<{ id: number, nomfichier: string, category: string, date: string }[]>([])
+ export default  useBdd = {
+  async function fetchAllData(bdd) {
+    try {
+       const result = await bdd.query('SELECT * FROM nom_fichier')
+       data.value = result.rows.map((row: any) => ({
+         id: row.id,
+         nomfichier: row.nomfichier,
+         category: row.category,
+         date: row.date,
+       }))
+       return data.value
+     } 
+     catch (error) {
+       console.error(error)
+       return []
+     }
+   },
 
-//   async function fetchAllData() {
-//     try {
-//       const result = await bdd.query('SELECT * FROM nom_fichier')
-//       data.value = result.rows.map((row: any) => ({
-//         id: row.id,
-//         nomfichier: row.nomfichier,
-//         category: row.category,
-//         date: row.date,
-//       }))
-//       return data.value
-//     } 
-//     catch (error) {
-//       console.error(error)
-//       return []
-//     }
-//   }
+   async function fetchData(query: string) {
+     try {
+       const result = await bdd.query(query)
+       return result.rows
+     }
+      catch (error) {
+       console.error(error)
+       return []
+     }
+   },
 
-//   async function fetchData(query: string) {
-//     try {
-//       const result = await bdd.query(query)
-//       return result.rows
-//     }
-//      catch (error) {
-//       console.error(error)
-//       return []
-//     }
-//   }
-
-//   return {
-//     data,
-//     fetchAllData,
-//     fetchData,
-//   }
-// }
+   return {
+     fetchAllData,
+     fetchData,
+   }
+ }
