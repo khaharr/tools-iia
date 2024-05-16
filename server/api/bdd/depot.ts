569eb9249f0e7a406073931e846d74de32ddc82e
Khlidbg78dem <khalidjj78200@icloud.com>
2024-05-16 22:42:37 +0200

    lo

--- server/api/bdd/depot.ts
+++ server/api/bdd/depot.ts
@@ -1,20 +1,36 @@
 import { defineEventHandler } from 'h3';
-import { readBody, readRawBody } from 'h3';
+import { readMultipartFormData } from 'h3';
 import fs from 'fs';
 import path from 'path';
 
 export default defineEventHandler(async (event) => {
-  const formData = await readMultipartFormData(event) 
-  if (formData === undefined){
-    return "error"
-  };
-
-  formData.forEach(file => {
-    const fileBuffer: Buffer = file.data  
-    const fileContent = fileBuffer.toString('utf8'); // converti
-    console.log(file.filename , fileContent); 
+  const formData = await readMultipartFormData(event);
   
-  });
+  if (formData === undefined || formData.length === 0) {
+    return { message: 'Aucun fichier trouvé.' };
+  }
+ 
+  try {
+    const uploadsDirectory = path.join(process.cwd(), 'server','api', 'bdd', 'uploads');
 
-  return { message: 'Fichiers envoyés avec succès!' };
+    for (const file of formData) {
+      const fileBuffer: Buffer = file.data;
+      const fileContent = fileBuffer.toString('utf8');
+      
+      if (file.filename === undefined){
+        return "error"
+      };
+      const filePath = path.join(uploadsDirectory, file.filename);
+
+      // Écrire le contenu du fichier dans le fichier de destination
+      fs.writeFileSync(filePath, fileContent, 'utf8');
+      
+      console.log(`Fichier enregistré: ${file.filename}`);
+    }
+
+    return { message: 'Fichiers envoyés avec succès!' };
+  } catch (error) {
+    console.error('Erreur lors de l\'enregistrement des fichiers :', error);
+    return { message: 'Erreur lors de l\'enregistrement des fichiers.' };
+  }
 });
