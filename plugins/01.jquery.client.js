64b655adc490282e02c9d2bbc36091b3363766ba
brahimS78 <brahim.sisi78@gmail.com>
2024-05-24 14:43:37 +0200

    Update 01.jquery.client.js

--- plugins/01.jquery.client.js
+++ plugins/01.jquery.client.js
@@ -2,3 +2,11 @@ import $ from 'jquery'
 
 window.jQuery = window.$ = $
 export default window.jQuery
+
+
+{
+  "extends": "../.nuxt/tsconfig.server.json",
+  "exclude": [
+    "../plugins/*.js"
+  ]
+}
