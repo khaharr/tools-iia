7dd94520252993c68565eba3dadc3e2e42889ff3
brahimS78 <brahim.sisi78@gmail.com>
2024-05-24 13:38:52 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,11 @@
 //https://nuxt.com/docs/api/nuxt-config
 export default defineNuxtConfig({
-  ssr:false,
+  ssr:true,
+  devtools: { enabled: true },
   css: ["bootstrap/dist/css/bootstrap.min.css",
   "bootstrap-icons/font/bootstrap-icons.min.css",
   "bootstrap-table/dist/bootstrap-table.min.css"
-  ], // add
-  vite: {
-    define: {
-      "process.env.DEBUG": true,
-    },
-  },
-  
-  app: {
-    
-    pageTransition: { name: 'page', mode: 'out-in' },
-    head: {
-      script: [
-        {
-          
-          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
-          
-        },
-      ],
-    },
-  },
+  ]
 });
 
 
