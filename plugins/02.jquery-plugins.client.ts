c40605382c7afc910a56205005fc07b9101b9c92
brahimS78 <brahim.sisi78@gmail.com>
2024-05-24 13:35:22 +0200

    Create 02.jquery-plugins.client.ts

---
+++ plugins/02.jquery-plugins.client.ts
@@ -0,0 +1,7 @@
+import 'tableexport.jquery.plugin/tableExport.min.js'
+import 'tableexport.jquery.plugin/libs/jsPDF/jspdf.umd.min.js'
+import 'tableexport.jquery.plugin/libs/FileSaver/FileSaver.min.js'
+import 'tableexport.jquery.plugin/libs/html2canvas/html2canvas.min.js'
+import 'tableexport.jquery.plugin/libs/js-xlsx/xlsx.core.min.js'
+
+export default defineNuxtPlugin({})
